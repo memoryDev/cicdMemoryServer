979fd9202cc8c540208c4862ae19c48ed71fea1b
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-12-29 20:35:05 +0900

    first commit

---
+++ src/main/java/dev/memory/memoryserver/MemoryServerApplication.java
@@ -0,0 +1,13 @@
+package dev.memory.memoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryServerApplication.class, args);
+    }
+
+}
